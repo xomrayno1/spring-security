e7f5420a229997be1ba4d5c93b31d6338b7862a9
Tam Nguyen <tamnct1999@gmail.com>
2025-01-30 00:02:55 +0700

    first commit

---
+++ src/main/java/com/app/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests()
+			.requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
+			.requestMatchers("/notices", "/contact").permitAll()
+			.and().formLogin()
+			.and().httpBasic();
+		return http.build();
+	}
+
+}
---
+++ src/main/java/com/app/controller/AccountController.java
@@ -0,0 +1,14 @@
+package com.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+	@GetMapping("/myAccount")
+	public String getAccountDetails() {
+		return "Welcome to Account Page";
+	}
+	
+}
